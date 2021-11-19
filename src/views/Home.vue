c7cf1092bc94721387ecb53fa14a89ef09bcb2ac
gsakshay <gsakshay20002002@gmail.com>
2021-11-19 18:41:17 +0530

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h2 @click="goToAbout">HELLO WORLD</h2>
+    <p>I am gonna build an awesome application</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+  },
+  methods: {
+    goToAbout(){
+      this.$router.push('about')
+    }
+  }
+});
+</script>
